c12cd906d395e20a4fe683f3659b92ad039b6d89
cscarioni <cscarioni@cscarioni-ubuntu-desktop.(none)>
2013-02-24 21:58:02 +0000

    fixed some issues storing json cards

--- public/javascripts/task_board.js
+++ public/javascripts/task_board.js
@@ -1,17 +1,27 @@
 var Card = function (config) {
     this.position = config.position
-    this.html = "<p class='sticky taped2' style='width: 150px;'>" +
-        "<strong>The Little Turtle</strong>.<br />" +
-        "There was a little turtle.<br />" +
+    this.html = "<p id='" + config.id + "'class='sticky taped' style='width: 150px;'>" +
+        "<strong>" + config.name + "</strong>.<br />" +
+        config.description + "<br />" +
         "</p>";
 
+
+    this.id = config.id;
+    this.name = config.name;
+    this.description = config.description;
+    this.position = config.position;
+
+
     this.updatePosition = function (position) {
+        console.log(position);
+        if (position == undefined){
+            position = this.position;
+        }
         this.position = position
-        this.x = position.left;
-        $(".sticky").offset(position);
-        console.log("moving to: " + JSON.stringify(position));
+        $("#"+this.id).offset(this.position);
         return this;
-    }
+    };
+
 };
 
 var TaskBoard = function (socket) {
@@ -19,11 +29,9 @@ var TaskBoard = function (socket) {
 };
 
 TaskBoard.prototype.cardMoved = function (card) {
-    var message = {
-        card: card
-    };
-    this.socket.emit('card', card);
+    var clonedCard = JSON.parse(JSON.stringify(card));
+    clonedCard.position = absolutePosition(card.position);
+    this.socket.emit('card', clonedCard);
 };
 
-
-var card = new Card({position: {top: 0, left: 0}});
+var cards = {};
--- public/javascripts/task_board_ui.js
+++ public/javascripts/task_board_ui.js
@@ -3,16 +3,62 @@ $(document).ready(function () {
     var taskBoard = new TaskBoard(socket);
     socket.on('card', function (result) {
         if (result.position !== undefined) {
-            card.updatePosition(result.position);
+            cards[result.id].updatePosition(relativizePosition(result.position));
         } else {
             console.log("Error result doesn' have position coordinates" + JSON.stringify(result));
         }
     });
 
-    $("#stories-column").append(card.html);
-    $(".sticky").draggable({
-        drag: function (event, ui) {
-          taskBoard.cardMoved(card.updatePosition(ui.position));
+    socket.on("sprintJoined", function (sprint) {
+        sprint.tasks.forEach(function (task) {
+            var card = new Card(task);
+            $("#stories-column").append(card.html);
+            card.updatePosition(relativizePosition(task.position));
+            makeDraggable(card);
+            cards[task.id] = card;
+        });
+
+    });
+
+    function makeDraggable(card) {
+        $("#" + card.id).draggable({
+            drag: function (event, ui) {
+                console.log("DRAGGED " + JSON.stringify(ui.position));
+                taskBoard.cardMoved(card.updatePosition(ui.position));
+            }
+        });
+    }
+
+    $("#commander").keypress(function (event) {
+        if (event.which == 13) {
+            socket.emit("join", $("#commander").val());
         }
     });
 });
+
+var browserWidth = 0;
+var browserHeight = 0;
+
+
+function jqUpdateSize(){
+    browserWidth = $(window).width();     // Display the width
+    browserHeight = $(window).height();    // Display the height
+    console.log(browserWidth + "x" + browserHeight);
+}
+
+function relativizePosition(position){
+    return {
+        top: position.top,
+        left: position.left * browserWidth / 1905
+    }
+}
+
+function absolutePosition(position){
+    return {
+        top: position.top,
+        left: position.left * 1905 / browserWidth
+    }
+}
+
+$(document).ready(jqUpdateSize);    // When the page first loads
+$(window).resize(jqUpdateSize);
