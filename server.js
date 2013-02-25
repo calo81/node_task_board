8c22c6ec44c013fbd18612749907e31cc625a85c
Carlo <carlo.scarioni@gmail.com>
2013-02-25 13:27:36 +0000

    changed listening port to 8720

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var server = http.createServer(function (request, response) {
     serveStatic(response, cache, absPath);
 });
 
-server.listen(3001, function () {
+server.listen(8720, function () {
     console.log("Server listening on port 3001.");
 });
 
