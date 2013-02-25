8597a1714c9f3ed78f55c9e084677a769d88158b
Carlo <carlo.scarioni@gmail.com>
2013-02-25 12:30:35 +0000

    added header. Also support for multiple sprints

--- public/javascripts/task_board.js
+++ public/javascripts/task_board.js
@@ -1,27 +1,65 @@
 var Card = function (config) {
-    this.position = config.position
-    this.html = "<p id='" + config.id + "'class='sticky taped' style='width: 150px; position: absolute;'>" +
-        "<strong>" + config.name + "</strong>.<br />" +
-        config.description + "<br />" +
-        "</p>";
 
-
+    var me = this;
+    this.position = config.position;
     this.id = config.id;
     this.name = config.name;
     this.description = config.description;
-    this.position = config.position;
 
 
     this.updatePosition = function (position) {
         console.log(position);
-        if (position == undefined){
+        if (position == undefined) {
             position = this.position;
         }
         this.position = position;
-        console.log("The position is " + JSON.stringify(this.position));
-        $("#"+this.id).offset(this.position);
+        $("#" + this.id).offset(this.position);
         return this;
     };
+
+    this.renderIn = function (element) {
+        element.append(this.html());
+    }
+
+    this.makeDraggable = function () {
+        $("#" + this.id).draggable({
+            drag: function (event, ui) {
+                taskBoard.cardMoved(me.updatePosition(ui.position));
+            }
+        });
+    }
+
+    this.html = function () {
+        return "<p id='" + this.id + "'class='sticky taped' style='width: 150px; position: absolute;'>" +
+            "<strong>" + this.name + "</strong>.<br />" +
+            this.description + "<br />" +
+            "</p>";
+    };
+
+    this.formHtml = function () {
+        return "<div id='divForm" + this.id + "'>New Task: <input id='textForm" + this.id + "' type='text' name='title'/></div>";
+    };
+
+    this.renderFormIn = function (element) {
+        if(mySprint == undefined) {
+          alert("You have to select a Sprint");
+            return;
+        }
+
+        element.append(this.formHtml());
+        $("#textForm" + this.id).keypress(function (event) {
+            if (event.which == 13) {
+                var values = $("#textForm" + me.id).val().split("/");
+                console.log(values[0]);
+                me.name = values[0];
+                me.description = values[1];
+                $("#divForm" + me.id).remove();
+                me.renderIn(element);
+                me.makeDraggable();
+                taskBoard.cardMoved(me);
+            }
+        });
+    }
 
 };
 
--- public/javascripts/task_board_ui.js
+++ public/javascripts/task_board_ui.js
@@ -1,6 +1,8 @@
 var socket = io.connect();
+var taskBoard = new TaskBoard(socket);
+var mySprint = undefined;
+
 $(document).ready(function () {
-    var taskBoard = new TaskBoard(socket);
     socket.on('card', function (result) {
         if (result.position !== undefined) {
             cards[result.id].updatePosition(relativizePosition(result.position));
@@ -10,29 +12,34 @@ $(document).ready(function () {
     });
 
     socket.on("sprintJoined", function (sprint) {
+        $(".sticky").remove();
+        mySprint = sprint.company + " / " + sprint.name;
         sprint.tasks.forEach(function (task) {
             var card = new Card(task);
-            $("#stories-column").append(card.html);
+            card.renderIn($("#not-started-column"));
             card.updatePosition(relativizePosition(task.position));
-            makeDraggable(card);
+            card.makeDraggable();
             cards[task.id] = card;
         });
 
     });
 
-    function makeDraggable(card) {
-        $("#" + card.id).draggable({
-            drag: function (event, ui) {
-                console.log("DRAGGED " + JSON.stringify(ui.position));
-                taskBoard.cardMoved(card.updatePosition(ui.position));
-            }
-        });
-    }
 
     $("#commander").keypress(function (event) {
         if (event.which == 13) {
             socket.emit("join", $("#commander").val());
         }
+    });
+
+    $("#not-started-column").dblclick(function(){
+       var randomId=Math.floor(Math.random()*1000000000);
+       var card = new Card({
+           id: randomId,
+           position: {top: 60, left: 10},
+           name: "",
+           description: ""
+       });
+       card.renderFormIn($("#not-started-column"));
     });
 });
 
@@ -41,15 +48,22 @@ var browserHeight = 0;
 
 
 function jqUpdateSize(){
+    var previousWidth = browserWidth;
     browserWidth = $(window).width();     // Display the width
     browserHeight = $(window).height();    // Display the height
-    console.log(browserWidth + "x" + browserHeight);
+
+    for(var index in cards) {
+        cards[index].updatePosition(relativizePosition(cards[index].position, previousWidth));
+    }
 }
 
-function relativizePosition(position){
+function relativizePosition(position, previousWidth, previousHeight){
+    if(previousWidth == undefined){
+        previousWidth = 1905
+    }
     return {
         top: position.top,
-        left: position.left * browserWidth / 1905
+        left: position.left * browserWidth / previousWidth
     }
 }
 
